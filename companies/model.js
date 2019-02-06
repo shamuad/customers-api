0f7d2008fe1a7c4b4534a0cfa3b174d2a6aaeef3
ARI DINAR <dinarari@gmail.com>
2019-02-06 11:29:34 +0100

    create company

---
+++ companies/model.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize')
+const sequelize = require('../db')
+
+const Company = sequelize.define('companies', {
+  name: {
+    type: Sequelize.STRING,
+    field: 'name',
+    allowNull: false
+  },
+  foundingYear: {
+    type: Sequelize.INTEGER,
+    field: 'founding_year',
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: true
+  }
+}, {
+    timestamps: false,
+    tableName: 'companies'
+  })
+
+module.exports = Company
--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const customersRouter = require('./customers/routes')
+const companiesRouter = require('./companies/routes')
 
 const app = express()
 const port = process.env.PORT || 4000
 
 app
   .use(bodyParser.json())
-  .use(customersRouter)
+  .use(customersRouter, companiesRouter)
   .listen(port, () => console.log(`Listening on port ${port}`))
 
